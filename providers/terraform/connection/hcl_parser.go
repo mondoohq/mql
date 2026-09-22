mondoohq/cnquery

🐛 terraform: stop file discovery and tfvars loading from silently dropping data
Seven defects in the HCL connection, all of which produce wrong or missing
data with no error. Each is covered by a regression test that fails on main.

Silent data loss

- The vendored-module skip matched ".terraform" as a substring of the full
  walk path, so a scan root named `.terraform-configs/`, a repository
  directory called `.terraform-modules/`, or a file named `main.terraform.tf`
  was skipped entirely. The connection then reported an empty configuration
  and returned no error, so every policy over it passed vacuously. Matching
  is now on a whole path segment relative to the scan root.

- MODULE_EXAMPLES matched any `modules/<name>/examples/<case>/` path, which is
  the canonical layout of a first-party Terraform module repository. A
  repository's own examples, including any misconfiguration in them, were
  dropped. The pattern is now anchored on the `.terraform/` cache it was
  documented to target.

- filepath.WalkDir's return value was discarded, and a per-file parse failure
  returned an error from the callback. One unparseable .tf file therefore
  aborted the walk, the abort was thrown away, and every file ordered after
  the broken one vanished from a scan that reported success. Per-file
  failures now warn and continue; a real walk failure is returned.

- *.tfvars.json was parsed with the native HCL syntax parser. Terraform reads
  it as JSON, so every value in the file was dropped and each var.* reference
  silently fell back to the variable block's default. A security-relevant
  override declared in terraform.tfvars.json was invisible to a scan. JSON
  variable files now go through the JSON parser, and parse diagnostics are
  logged instead of discarded.

- Only the last .terraform/modules/modules.json the walk reached was kept, so
  a monorepo with one module cache per stack reported a single stack's
  modules. Records are now merged across manifests, deduplicated

providers/terraform/connection/hcl_parser.go
@@ -10,6 +10,8 @@ import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclparse"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/json"
+	"github.com/rs/zerolog/log"
 )
 
 func NewHCLFileLoader() *hclFileLoader {
@@ -24,19 +26,33 @@ type hclFileLoader struct {
 	hclParser *hclparse.Parser
 }
 
-// ParseHclFile parses a single terraform file
+// ParseHclFile parses a single terraform file.
+//
+// The source bytes are read here rather than handed to the parser's
+// *File variants on purpose. hclparse.Parser.ParseJSONFile records its result
+// in the parser's file map before checking it, and json.ParseFile returns a
+// nil *hcl.File when the file cannot be opened — so an unreadable `.tf.json`
+// (a dangling symlink in a checkout is enough) left a nil entry in
+// Parser().Files(). Every consumer of that map dereferences file.Body, so the
+// nil surfaced later as a provider panic that took down the whole scan.
+// ParseHCL/ParseJSON always store a real *hcl.File.
 func (h *hclFileLoader) ParseHclFile(filepath string) error {
-	var parseFunc func(filename string) (*hcl.File, hcl.Diagnostics)
+	var parseFunc func(src []byte, filename string) (*hcl.File, hcl.Diagnostics)
 	switch {
 	case strings.HasSuffix(filepath, ".tf"):
-		parseFunc = h.hclParser.ParseHCLFile
+		parseFunc = h.hclParser.ParseHCL
 	case strings.HasSuffix(filepath, ".tf.json"):
-		parseFunc = h.hclParser.ParseJSONFile
+		parseFunc = h.hclParser.ParseJSON
 	default:
 		return nil
 	}
 
-	_, diag := parseFunc(filepath)
+	src, err := os.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
+
+	_, diag := parseFunc(src, filepath)
 	if diag != nil && diag.HasErrors() {
 		return diag
 	}
@@ -47,28 +63,52 @@ func (h *hclFileLoader) GetParser() *hclparse.Parser {
 	return h.hclParser
 }
 
+// ReadTfVarsFromFile loads variable definitions from a `.tfvars` or
+// `.tfvars.json` file into terraformVars.
+//
+// The two formats need different parsers. Terraform reads `*.tfvars.json` as
+// JSON; running it through the native HCL syntax parser produces only syntax
+// errors, so every value in the file was silently dropped and each `var.*`
+// reference fell back to the variable block's default. A security-relevant
+// override declared in `terraform.tfvars.json` was therefore invisible to a
+// scan — a false negative with no error to notice.
 func ReadTfVarsFromFile(filename string, terraformVars map[string]*hcl.Attribute) error {
-	switch {
-	case strings.HasSuffix(filename, ".tfvars"):
-		fallthrough
-	case strings.HasSuffix(filename, ".tfvars.json"):
+	var variableFile *hcl.File
+	var diags hcl.Diagnostics
 
+	switch {
+	case strings.HasSuffix(filename, ".tfvars.json"):
 		src, err := os.ReadFile(filename)
 		if err != nil {
 			return err
 		}
-
-		// we ignore the diagnostics information here
-		variableFile, _ := hclsyntax.ParseConfig(src, filename, hcl.Pos{Line: 1, Column: 1})
-
-		// NOTE: we ignore the diagnostics info
-		attrs, _ := variableFile.Body.JustAttributes()
-		for k := range attrs {
-			v := attrs[k]
-			terraformVars[k] = v
+		variableFile, diags = json.Parse(src, filename)
+	case strings.HasSuffix(filename, ".tfvars"):
+		src, err := os.ReadFile(filename)
+		if err != nil {
+			return err
 		}
-		return nil
+		variableFile, diags = hclsyntax.ParseConfig(src, filename, hcl.Pos{Line: 1, Column: 1})
 	default:
 		return nil
 	}
+
+	// Parse diagnostics are not fatal — a partially readable tfvars file still
+	// contributes the attributes it did parse — but they must not vanish
+	// silently, or a typo'd file looks identical to an absent one.
+	if diags.HasErrors() {
+		log.Warn().Str("path", filename).Str("diagnostics", diags.Error()).Msg("could not fully parse tfvars file")
+	}
+	if variableFile == nil {
+		return nil
+	}
+
+	attrs, attrDiags := variableFile.Body.JustAttributes()
+	if attrDiags.HasErrors() {
+		log.Debug().Str("path", filename).Str("diagnostics", attrDiags.Error()).Msg("tfvars file has non-attribute content")
+	}
+	for k := range attrs {
+		terraformVars[k] = attrs[k]
+	}
+	return nil
 }

mondoohq/mql

providers/terraform/connection/hcl_parser.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclparse"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/json"
 	"github.com/rs/zerolog/log"
 )
 
@@ -26,25 +27,39 @@ type hclFileLoader struct {
 
 // ParseHclFile parses a single Terraform or OpenTofu configuration file
 // (.tf, .tf.json, .tofu or .tofu.json). Anything else is skipped.
+//
+// The source bytes are read here rather than handed to the parser's
+// *File variants on purpose. hclparse.Parser.ParseJSONFile records its result
+// in the parser's file map before checking it, and json.ParseFile returns a
+// nil *hcl.File when the file cannot be opened — so an unreadable `.tf.json`
+// (a dangling symlink in a checkout is enough) left a nil entry in
+// Parser().Files(). Every consumer of that map dereferences file.Body, so the
+// nil surfaced later as a provider panic that took down the whole scan.
+// ParseHCL/ParseJSON always store a real *hcl.File.
 func (h *hclFileLoader) ParseHclFile(path string) error {
 	f, ok := classifyConfigFile(path)
 	if !ok {
 		return nil
 	}
 
-	var parseFunc func(filename string) (*hcl.File, hcl.Diagnostics)
+	var parseFunc func(src []byte, filename string) (*hcl.File, hcl.Diagnostics)
 	switch f.class {
 	case classConfig:
-		parseFunc = h.hclParser.ParseHCLFile
+		parseFunc = h.hclParser.ParseHCL
 	case classConfigJSON:
-		parseFunc = h.hclParser.ParseJSONFile
+		parseFunc = h.hclParser.ParseJSON
 	default:
 		// a variables file; ReadTfVarsFromFile handles those
 		return nil
 	}
 
+	src, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
 	log.Debug().Str("path", path).Str("dialect", string(f.dialect)).Msg("parsing hcl file")
-	_, diag := parseFunc(path)
+	_, diag := parseFunc(src, path)
 	if diag != nil && diag.HasErrors() {
 		return diag
 	}
@@ -57,25 +72,55 @@ func (h *hclFileLoader) GetParser() *hclparse.Parser {
 
 // ReadTfVarsFromFile reads a variable definitions file (.tfvars, .tfvars.json,
 // .tofuvars or .tofuvars.json) into terraformVars. Anything else is skipped.
+//
+// The two formats need different parsers. Terraform reads `*.tfvars.json` as
+// JSON; running it through the native HCL syntax parser produces only syntax
+// errors, so every value in the file was silently dropped and each `var.*`
+// reference fell back to the variable block's default. A security-relevant
+// override declared in `terraform.tfvars.json` was therefore invisible to a
+// scan — a false negative with no error to notice.
 func ReadTfVarsFromFile(filename string, terraformVars map[string]*hcl.Attribute) error {
 	f, ok := classifyConfigFile(filename)
-	if !ok || (f.class != classVars && f.class != classVarsJSON) {
+	if !ok {
 		return nil
 	}
 
-	src, err := os.ReadFile(filename)
-	if err != nil {
-		return err
+	var variableFile *hcl.File
+	var diags hcl.Diagnostics
+
+	switch f.class {
+	case classVarsJSON:
+		src, err := os.ReadFile(filename)
+		if err != nil {
+			return err
+		}
+		variableFile, diags = json.Parse(src, filename)
+	case classVars:
+		src, err := os.ReadFile(filename)
+		if err != nil {
+			return err
+		}
+		variableFile, diags = hclsyntax.ParseConfig(src, filename, hcl.Pos{Line: 1, Column: 1})
+	default:
+		return nil
 	}
 
-	// we ignore the diagnostics information here
-	variableFile, _ := hclsyntax.ParseConfig(src, filename, hcl.Pos{Line: 1, Column: 1})
+	// Parse diagnostics are not fatal — a partially readable tfvars file still
+	// contributes the attributes it did parse — but they must not vanish
+	// silently, or a typo'd file looks identical to an absent one.
+	if diags.HasErrors() {
+		log.Warn().Str("path", filename).Str("diagnostics", diags.Error()).Msg("could not fully parse tfvars file")
+	}
+	if variableFile == nil {
+		return nil
+	}
 
-	// NOTE: we ignore the diagnostics info
-	attrs, _ := variableFile.Body.JustAttributes()
+	attrs, attrDiags := variableFile.Body.JustAttributes()
+	if attrDiags.HasErrors() {
+		log.Debug().Str("path", filename).Str("diagnostics", attrDiags.Error()).Msg("tfvars file has non-attribute content")
+	}
 	for k := range attrs {
-		v := attrs[k]
-		terraformVars[k] = v
+		terraformVars[k] = attrs[k]
 	}
 	return nil
 }